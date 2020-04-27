7750ffc94e7d13e9e7c3ed3d6f0f7af4b025c78c
zhangsa <sandy.zhang@tabcorp.com.au>
2020-04-27 10:28:49 +1000

    changes

--- LongestSubstringWithoutRepeatingCharacters.java
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -1,19 +1,20 @@
 import java.util.HashMap;
+import java.util.Map;
 
 // 3. Longest Substring Without Repeating Characters
 //  Sliding Window
 public class Solution {
   public int lengthOfLongestSubstring(String s) {
       int n = s.length(), ans = 0;
-      Map<Character, Interget> map = new HashMap<>();
+      Map<Character, Integer> map = new HashMap<>();
 
-      for(j=0, i=0; j<n; j++){
+      for(int j=0, i=0; j<n; j++){
         if(map.containsKey(s.charAt(j))){
           i = Math.max(i, map.get(s.charAt(j)));
         }
 
         ans = Math.max(ans, j-i +1) ;
-        map.put(s.charAt(j), j+1)
+        map.put(s.charAt(j), j+1);
       }
 
       return ans;
--- MergeKSortedLists.java
+++ MergeKSortedLists.java
@@ -3,19 +3,13 @@ import java.util.PriorityQueue;
 public class MergeKSortedLists {
   public ListNode mergeKLists(ListNode[] lists) {
     PriorityQueue<Integer> pq = new PriorityQueue<>();
-    int len = lists.length-1; // last head index
-    while(len>=0) {
-        for(int i=0; i<=len; i++) {
-        	if(lists[i]!=null) {
-        		pq.add(lists[i].val); // add current head into the priority queue
-        		lists[i] = lists[i].next; // move current head to head's next node
-        	}
-        	else {
-        		lists[i] = lists[len--]; // switch null head with the last non-null head
-        	}
+    for(ListNode head : lists){
+        while(head != null){
+            pq.add(head.val);
+            head = head.next;
         }
     }
-
+    
 	ListNode dummy = new ListNode(-1);
 	ListNode curr = dummy;
     while(!pq.isEmpty()) { // convert pq into linked list
